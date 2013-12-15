1d013c19b7e44416f85be2be8da408fb8336139c
itsbalamurali <balamurali.pandranki@gmail.com>
2013-12-15 12:18:20 +0530

    viewport

--- php/models/header.php
+++ php/models/header.php
@@ -16,7 +16,7 @@ echo"
 <html lang='en'>
 <head lang='en'>
 <meta charset='utf-8'>
-<meta name='viewport' content='initial-scale=1'>
+<meta name='viewport' content='width=device-width, initial-scale=1'>
 <meta name='description' content=''>
 <meta name='author' content=''>
 <!-- Favicons -->
